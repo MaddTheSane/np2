aaf11ecd7342e3b644af55268e52a2fdbedf83b8
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2003-12-10 12:48:09 +0000

    add ITF Images (T.Yui)
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@257 7c2116fe-53d0-4aec-ab59-80607f05abc0

---
+++ romimage/tool/BIN2TXT.C
@@ -0,0 +1,55 @@
+#include	<stdio.h>
+#include	<string.h>
+#include	"common.h"
+#include	"dosio.h"
+
+// ----------------------------------------- メイン
+
+void main(int argc, BYTE *argv[], BYTE *envp[]) {
+
+	FILEH	fh;
+	BYTE	buf[12];
+	BYTE	out[256];
+	WORD	i;
+	WORD	j;
+
+	if (argc < 2) {
+		printf("ファイルを指定して下さい.\n");
+		return;
+	}
+	if ((fh = file_open(argv[1])) == -1) {
+		printf("ファイルが見つかりません.\n");
+		return;
+	}
+
+	if (argc >= 3) {
+		printf("static unsigned char %s[] = {\n", argv[2]);
+	}
+	else {
+		int		i;
+		for (i=0; ; i++) {
+			if (!argv[1][i]) {
+				break;
+			}
+			if (argv[1][i] == '.') {
+				break;
+			}
+			out[i] = argv[1][i];
+		}
+		out[i] = 0;
+		printf("static unsigned char %s[] = {\n", out);
+	}
+
+	while(1) {
+		j = file_read(fh, buf, 12);
+		if ((j == 0) || (j == -1)) {
+			break;
+		}
+		for (i=0; i<j; i++) {
+			sprintf(&out[i*5], "0x%02x,", buf[i]);
+		}
+		printf("\t\t\t%s\n", out);
+	}
+	file_close(fh);
+	printf("};\n\n");
+}
