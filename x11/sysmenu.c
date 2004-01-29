419476bce3d3a73e7a8f16ae1f5c0b93d1870183
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2004-01-29 11:44:19 +0000

    sync with sdl
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@488 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- x11/sysmenu.c
+++ x11/sysmenu.c
@@ -77,6 +77,40 @@ sys_cmd(MENUID id)
 		diskdrv_sethdd(0x01, NULL);
 		break;
 
+#if defined(SUPPORT_SCSI)
+	case MID_SCSI0OPEN:
+		filesel_hdd(0x20);
+		break;
+
+	case MID_SCSI0EJECT:
+		diskdrv_sethdd(0x20, NULL);
+		break;
+
+	case MID_SCSI1OPEN:
+		filesel_hdd(0x21);
+		break;
+
+	case MID_SCSI1EJECT:
+		diskdrv_sethdd(0x21, NULL);
+		break;
+
+	case MID_SCSI2OPEN:
+		filesel_hdd(0x22);
+		break;
+
+	case MID_SCSI2EJECT:
+		diskdrv_sethdd(0x22, NULL);
+		break;
+
+	case MID_SCSI3OPEN:
+		filesel_hdd(0x23);
+		break;
+
+	case MID_SCSI3EJECT:
+		diskdrv_sethdd(0x23, NULL);
+		break
+#endif
+
 	case MID_DISPSYNC:
 		np2cfg.DISPSYNC ^= 1;
 		update |= SYS_UPDATECFG;
