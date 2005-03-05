969ad638bcd17ceefec6f3978177fa7dc5705c20
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2005-03-05 13:48:19 +0000

    remove unsed function.
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@918 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- x11/oemtext.c
+++ x11/oemtext.c
@@ -1,18 +1,3 @@
 #include "compiler.h"
 
 #include "oemtext.h"
-
-
-UINT
-oemtext_sjistooem(OEMCHAR *dst, UINT dcnt, const char *src, UINT scnt)
-{
-
-	return 0;
-}
-
-UINT
-oemtext_oemtosjis(char *dst, UINT dcnt, const OEMCHAR *src, UINT scnt)
-{
-
-	return 0;
-}
