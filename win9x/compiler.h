d0c3fec7e6ef9e16d625a20230c65af0d3acf52c
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2008-02-17 01:29:17 +0000

    fix windows resource, romeo
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1141 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- win9x/compiler.h
+++ win9x/compiler.h
@@ -147,9 +147,9 @@ typedef	signed __int64		SINT64;
 #define	SUPPORT_HOSTDRV
 #define	SUPPORT_SASI
 #define	SUPPORT_SCSI
-#if defined(TRACE)
+// #if defined(TRACE)
 #define	SUPPORT_IDEIO
-#endif
+// #endif
 #define SUPPORT_ARC
 #define SUPPORT_ZLIB
 #if !defined(_WIN64)
