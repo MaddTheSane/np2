78d8bebbcad2e6c0df88f2269dbdac94aaf37b1c
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2011-12-16 12:04:01 +0000

    unzip: do overwrite.
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1217 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- x11/autogen.sh
+++ x11/autogen.sh
@@ -5,5 +5,5 @@ automake -aci --foreign
 autoconf
 rm -f config.h.in~
 ./configure "$*"
-( cd ../np2tool && unzip -j -f np2tool.zip ) 2>&1 > /dev/null
+( cd ../np2tool && unzip -j -o np2tool.zip ) 2>&1 > /dev/null
 make maintainer-clean 2>&1 > /dev/null
