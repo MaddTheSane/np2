6c0565ae20ab0c8a29418eac77d7e79a7256ead7
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2011-12-24 17:48:04 +0000

    versioning
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1285 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- np2ver.h
+++ np2ver.h
@@ -4,7 +4,7 @@
 // #define	NP2VER_WIN9X
 // #define	NP2VER_MACOS9
 // #define	NP2VER_MACOSX
-#define	NP2VER_X11	"-20111224"
+// #define	NP2VER_X11
 // #define	NP2VER_WINCE
 // #define	NP2VER_SDLZAU
 
