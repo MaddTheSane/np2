9903886d52e73985495b7c797c78e562028bd344
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2012-01-23 03:57:31 +0000

    fix format string.
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1325 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- x11/gtk2/gtk_main.c
+++ x11/gtk2/gtk_main.c
@@ -419,7 +419,7 @@ gui_gtk_msgbox(const char *title, const char *msg, UINT flags)
 
 	dialog = gtk_message_dialog_new(GTK_WINDOW(main_window),
 	    GTK_DIALOG_DESTROY_WITH_PARENT|GTK_DIALOG_MODAL,
-	    msgtype, btntype, msg);
+	    msgtype, btntype, "%s", msg);
 	gtk_window_set_title(GTK_WINDOW(dialog), title);
 
 	gtk_widget_show_all(dialog);
