1b72982221a54c903beeada791c6a2fea14b2899
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2012-01-24 15:52:06 +0000

    FWAIT can't cause NM_EXCEPTION...
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1351 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- i386c/ia32/instructions/fpu/fpdummy.c
+++ i386c/ia32/instructions/fpu/fpdummy.c
@@ -7,28 +7,23 @@
 void
 FWAIT(void)
 {
-#if !defined(USE_FPU)
-	EXCEPTION(NM_EXCEPTION, 0);
-#else
-//	TRACEOUT(("use FPU - FWAIT"));
-#endif
+
+	TRACEOUT(("use FPU - FWAIT"));
 }
 
 void
 ESC0(void)
 {
-#if !defined(USE_FPU)
-	EXCEPTION(NM_EXCEPTION, 0);
-#else
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU d8 %.2x", op));
+	TRACEOUT(("use FPU d8 %.2x", op));
 	if (op >= 0xc0) {
+		EXCEPTION(NM_EXCEPTION, 0);
 	} else {
 		madr = calc_ea_dst(op);
+		EXCEPTION(NM_EXCEPTION, 0);
 	}
-#endif
 }
 
 void
@@ -37,21 +32,9 @@ ESC1(void)
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU d9 %.2x", op));
-#if !defined(USE_FPU)
+	TRACEOUT(("use FPU d9 %.2x", op));
 	if (op >= 0xc0) {
 		EXCEPTION(NM_EXCEPTION, 0);
-	} else {
-		madr = calc_ea_dst(op);
-		if (((op >> 3) & 7) != 7) {
-			EXCEPTION(NM_EXCEPTION, 0);
-		}
-		/* FSTCW */
-		TRACEOUT(("FSTCW"));
-		cpu_vmemorywrite_w(CPU_INST_SEGREG_INDEX, madr, 0xffff);
-	}
-#else
-	if (op >= 0xc0) {
 	} else {
 		madr = calc_ea_dst(op);
 		if ((op & 0x38) == 0x38) {
@@ -59,24 +42,21 @@ ESC1(void)
 			cpu_vmemorywrite_w(CPU_INST_SEGREG_INDEX, madr, 0xffff);
 		}
 	}
-#endif
 }
 
 void
 ESC2(void)
 {
-#if !defined(USE_FPU)
-	EXCEPTION(NM_EXCEPTION, 0);
-#else
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU da %.2x", op));
+	TRACEOUT(("use FPU da %.2x", op));
 	if (op >= 0xc0) {
+		EXCEPTION(NM_EXCEPTION, 0);
 	} else {
 		madr = calc_ea_dst(op);
+		EXCEPTION(NM_EXCEPTION, 0);
 	}
-#endif
 }
 
 void
@@ -85,36 +65,32 @@ ESC3(void)
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU db %.2x", op));
-#if !defined(USE_FPU)
-	if (op != 0xe3) {
-		EXCEPTION(NM_EXCEPTION, 0);
-	}
-	/* FNINIT */
-	(void)madr;
-#else
+	TRACEOUT(("use FPU db %.2x", op));
 	if (op >= 0xc0) {
+		if (op != 0xe3) {
+			EXCEPTION(NM_EXCEPTION, 0);
+		}
+		/* FNINIT */
+		(void)madr;
 	} else {
 		madr = calc_ea_dst(op);
+		EXCEPTION(NM_EXCEPTION, 0);
 	}
-#endif
 }
 
 void
 ESC4(void)
 {
-#if !defined(USE_FPU)
-	EXCEPTION(NM_EXCEPTION, 0);
-#else
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU dc %.2x", op));
+	TRACEOUT(("use FPU dc %.2x", op));
 	if (op >= 0xc0) {
+		EXCEPTION(NM_EXCEPTION, 0);
 	} else {
 		madr = calc_ea_dst(op);
+		EXCEPTION(NM_EXCEPTION, 0);
 	}
-#endif
 }
 
 void
@@ -123,42 +99,33 @@ ESC5(void)
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU dd %.2x", op));
-#if !defined(USE_FPU)
+	TRACEOUT(("use FPU dd %.2x", op));
 	if (op >= 0xc0) {
 		EXCEPTION(NM_EXCEPTION, 0);
 	} else {
+		madr = calc_ea_dst(op);
 		if (((op >> 3) & 7) != 7) {
 			EXCEPTION(NM_EXCEPTION, 0);
 		}
 		/* FSTSW */
-		madr = calc_ea_dst(op);
 		TRACEOUT(("FSTSW"));
-		cpu_vmemorywrite_w(CPU_INST_SEGREG_INDEX, madr, 0);
+		cpu_vmemorywrite_w(CPU_INST_SEGREG_INDEX, madr, 0xffff);
 	}
-#else
-	if (op >= 0xc0) {
-	} else {
-		madr = calc_ea_dst(op);
-	}
-#endif
 }
 
 void
 ESC6(void)
 {
-#if !defined(USE_FPU)
-	EXCEPTION(NM_EXCEPTION, 0);
-#else
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU de %.2x", op));
+	TRACEOUT(("use FPU de %.2x", op));
 	if (op >= 0xc0) {
+		EXCEPTION(NM_EXCEPTION, 0);
 	} else {
 		madr = calc_ea_dst(op);
+		EXCEPTION(NM_EXCEPTION, 0);
 	}
-#endif
 }
 
 void
@@ -167,23 +134,16 @@ ESC7(void)
 	UINT32 op, madr;
 
 	GET_PCBYTE(op);
-//	TRACEOUT(("use FPU df %.2x", op));
-#if !defined(USE_FPU)
+	TRACEOUT(("use FPU df %.2x", op));
 	if (op >= 0xc0) {
 		if (op != 0xe0) {
 			EXCEPTION(NM_EXCEPTION, 0);
 		}
 		/* FSTSW AX */
 		TRACEOUT(("FSTSW AX"));
-		CPU_AX = 0;
+		CPU_AX = 0xffff;
 	} else {
 		madr = calc_ea_dst(op);
 		EXCEPTION(NM_EXCEPTION, 0);
 	}
-#else
-	if (op >= 0xc0) {
-	} else {
-		madr = calc_ea_dst(op);
-	}
-#endif
 }
