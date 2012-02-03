260ba5b51f018329d85ad2a3cdc7a85c3332043a
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2012-02-03 16:51:38 +0000

    do progress.
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1379 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- x11/00release.sh
+++ x11/00release.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+echo generating configure...
 sh autogen.sh
+
+echo applying patches...
 for i in 00patch-*
 do
-  ( cd .. && cat x11/$i | patch -p0 )
+  ( cd .. && cat x11/$i | patch -p0 -s )
 done
 ( cd .. && find . -type f -name "*.orig" | xargs rm -f )
+
+echo copying win9x np21 project files...
 cp -p ~/work/xnp2/x11/np21.* ../win9x/
+
 rm -f 00patch-*
 rm -f 00release.sh
