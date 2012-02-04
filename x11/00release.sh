9bd07a272228965e83069415417285e9e2bc28da
monaka <monaka@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2012-02-04 09:58:39 +0000

    remove patch file.
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@1382 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- x11/00release.sh
+++ x11/00release.sh
@@ -2,12 +2,12 @@
 echo generating configure...
 sh autogen.sh
 
-echo applying patches...
-for i in 00patch-*
-do
-  ( cd .. && cat x11/$i | patch -p0 -s )
-done
-( cd .. && find . -type f -name "*.orig" | xargs rm -f )
+#echo applying patches...
+#for i in 00patch-*
+#do
+#  ( cd .. && cat x11/$i | patch -p0 -s )
+#done
+#( cd .. && find . -type f -name "*.orig" | xargs rm -f )
 
 echo copying win9x np21 project files...
 cp -p ~/work/xnp2/x11/np21.* ../win9x/
