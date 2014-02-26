a67b28ac084885b9065eeabe167d5b3e44e3e7b0
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2014-02-26 13:34:06 +0000

    ✓ ROMEO: c++にする
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1426 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- WORK_01/win9x/juliet.cpp
+++ WORK_01/win9x/juliet.cpp
@@ -1,3 +1,8 @@
+/**
+ * @file	juliet.cpp
+ * @brief	ROMEO アクセス クラス
+ */
+
 #include	"compiler.h"
 
 #if defined(SUPPORT_ROMEO)
@@ -5,265 +10,389 @@
 #include	"romeo.h"
 #include	"juliet.h"
 
+/**
+ * @brief ROMEO アクセス クラス
+ */
+class CJuliet
+{
+public:
+	CJuliet();
+	bool Initialize();
+	void Deinitialize();
+	bool IsEnabled() const;
+	bool IsBusy() const;
+	void Reset() const;
+	void WriteRegister(UINT nAddr, UINT8 cData);
+	void WriteExtendRegister(UINT nAddr, UINT8 cData);
+	void Mute(bool bMute) const;
 
-enum {
-	ROMEO_AVAIL			= 0x01,
-	ROMEO_YMF288		= 0x02,
-	ROMEO_YM2151		= 0x04
+public:
+	HMODULE m_hModule;														//!< モジュール
+	ULONG (WINAPI * m_fnRead32)(ULONG ulPciAddress, ULONG ulRegAddress);	//!< コンフィグレーション読み取り関数
+	VOID (WINAPI * m_fnOut8)(ULONG ulAddress, UCHAR ucParam);				//!< outp 関数
+	VOID (WINAPI * m_fnOut32)(ULONG ulAddress, ULONG ulParam);				//!< outpd 関数
+	UCHAR (WINAPI * m_fnIn8)(ULONG ulAddress);								//!< inp 関数
+	bool m_bOpna;															//!< OPNA 有効フラグ
+	UCHAR m_ucIrq;															//!< ROMEO IRQ
+	UINT8 m_cPsgMix;														//!< PSG ミキサー
+	ULONG m_ulAddress;														//!< ROMEO ベース アドレス
+	UINT8 m_cAlgorithm[8];													//!< アルゴリズム テーブル
+	UINT8 m_cTtl[8 * 4];													//!< TTL テーブル
+
+	void Clear();
+	ULONG SearchRomeo() const;
+	void WriteRegisterInner(UINT8 cAddr, UINT8 cData) const;
+	void WriteExtendRegisterInner(UINT8 cAddr, UINT8 cData) const;
+	void SetVolume(UINT nChannel, int nVolume) const;
 };
 
-typedef struct {
-	HMODULE	mod;
+//! アルゴリズム スロット マスク
+static const UINT8 s_opmask[] = {0x08, 0x08, 0x08, 0x08, 0x0c, 0x0e, 0x0e, 0x0f};
 
-	ULONG	(WINAPI * finddev)(ULONG ven, ULONG dev, ULONG index);
-	ULONG	(WINAPI * read32)(ULONG pciaddr, ULONG regaddr);
-	void	(WINAPI * out8)(ULONG addr, UCHAR param);
-	void	(WINAPI * out16)(ULONG addr, USHORT param);
-	void	(WINAPI * out32)(ULONG addr, ULONG param);
-	UCHAR	(WINAPI * in8)(ULONG addr);
-	USHORT	(WINAPI * in16)(ULONG addr);
-	ULONG	(WINAPI * in32)(ULONG addr);
+//! @brief ロード関数
+struct ProcItem
+{
+	LPCSTR lpSymbol;		//!< 関数名
+	size_t nOffset;			//!< オフセット
+};
 
-	ULONG	addr;
-	ULONG	irq;
-	ULONG	avail;
+//! ロード関数リスト
+static const ProcItem s_dllProc[] =
+{
+	{FN_PCICFGREAD32,	offsetof(CJuliet, m_fnRead32)},
+	{FN_PCIMEMWR8,		offsetof(CJuliet, m_fnOut8)},
+	{FN_PCIMEMWR32,		offsetof(CJuliet, m_fnOut32)},
+	{FN_PCIMEMRD8,		offsetof(CJuliet, m_fnIn8)},
+};
 
-	UINT8	algo[8];
-	UINT8	ttl[8*4];
-	UINT8	psgmix;
-} _ROMEO;
+/**
+ * コンストラクタ
+ */
+CJuliet::CJuliet()
+{
+	Clear();
+}
 
+/**
+ * クリア
+ */
+void CJuliet::Clear()
+{
+	ZeroMemory(this, sizeof(*this));
+	FillMemory(m_cTtl, sizeof(m_cTtl), 0x7f);
+	m_cPsgMix = 0x3f;
+}
 
-static const UINT8 opmask[] = {0x08,0x08,0x08,0x08,0x0c,0x0e,0x0e,0x0f};
+/**
+ * 初期化
+ * @retval true 成功
+ * @retval false 失敗
+ */
+bool CJuliet::Initialize()
+{
+	Deinitialize();
 
-typedef struct {
-const char	*symbol;
-	UINT	addr;
-} DLLPROC;
+	m_hModule = ::LoadLibrary(PCIDEBUG_DLL);
+	if (m_hModule == NULL)
+	{
+		return false;
+	}
 
-static const DLLPROC dllproc[] = {
-				{FN_PCIFINDDEV,		offsetof(_ROMEO, finddev)},
-				{FN_PCICFGREAD32,	offsetof(_ROMEO, read32)},
-				{FN_PCIMEMWR8,		offsetof(_ROMEO, out8)},
-				{FN_PCIMEMWR16,		offsetof(_ROMEO, out16)},
-				{FN_PCIMEMWR32,		offsetof(_ROMEO, out32)},
-				{FN_PCIMEMRD8,		offsetof(_ROMEO, in8)},
-				{FN_PCIMEMRD16,		offsetof(_ROMEO, in16)},
-				{FN_PCIMEMRD32,		offsetof(_ROMEO, in32)}};
+	for (size_t i = 0; i < NELEMENTS(s_dllProc); i++)
+	{
+		FARPROC proc = ::GetProcAddress(m_hModule, s_dllProc[i].lpSymbol);
+		if (proc == NULL)
+		{
+			Deinitialize();
+			return false;
+		}
+		*(reinterpret_cast<FARPROC*>(reinterpret_cast<INTPTR>(this) + s_dllProc[i].nOffset)) = proc;
+	}
 
+	const ULONG ulPciAddress = SearchRomeo();
+	if (ulPciAddress == static_cast<ULONG>(-1))
+	{
+		Deinitialize();
+		return false;
+	}
+	m_ulAddress = (*m_fnRead32)(ulPciAddress, ROMEO_BASEADDRESS1);
+	m_ucIrq = static_cast<UCHAR>((*m_fnRead32)(ulPciAddress, ROMEO_PCIINTERRUPT));
+	if (!m_ulAddress)
+	{
+		Deinitialize();
+		return false;
+	}
 
-static	_ROMEO		romeo = {NULL};
+	m_bOpna = true;
+	Reset();
 
+	TRACEOUT(("ROMEO enable"));
+	return true;
+}
 
+/**
+ * 解放
+ */
+void CJuliet::Deinitialize()
+{
+	if (m_hModule)
+	{
+		::FreeLibrary(m_hModule);
+	}
+	Clear();
+}
 
-// pciFindPciDevice使うと、OS起動後一発目に見つけられないことが多いので、
-// 自前で検索する（矢野さん方式）
+/**
+ * ROMEO を探す
+ * @note pciFindPciDevice使うと、OS起動後一発目に見つけられないことが多いので、自前で検索する（矢野さん方式）
+ * @return バス アドレス
+ */
+ULONG CJuliet::SearchRomeo() const
+{
+	#define PCIBUSDEVFUNC(b, d, f)	(((b) << 8) | ((d) << 3) | (f))
+	#define	DEVVEND(v, d)			((ULONG)((v) | ((d) << 16)))
 
-#define PCIBUSDEVFUNC(b, d, f)	(((b) << 8) | ((d) << 3) | (f))
-#define	DEVVEND(v, d)			((ULONG)((v) | ((d) << 16)))
-
-static ULONG searchRomeo(void) {
-
-	UINT	bus;
-	UINT	dev;
-	UINT	func;
-	ULONG	addr;
-	ULONG	dev_vend;
-
-	for (bus=0; bus<0x100; bus++) {
-		for (dev=0; dev<0x20; dev++) {
-			for (func=0; func<0x08; func++) {
-				addr = PCIBUSDEVFUNC(bus, dev, func);
-				dev_vend = romeo.read32(addr, 0x0000);
-				if ((dev_vend == DEVVEND(ROMEO_VENDORID, ROMEO_DEVICEID)) ||
-					(dev_vend == DEVVEND(ROMEO_VENDORID, ROMEO_DEVICEID2))) {
-					return(addr);
+	for (UINT bus = 0; bus < 0x100; bus++)
+	{
+		for (UINT dev = 0; dev < 0x20; dev++)
+		{
+			for (UINT func = 0; func < 0x08; func++)
+			{
+				const ULONG ulPciAddress = PCIBUSDEVFUNC(bus, dev, func);
+				const ULONG ulDeviceVendor = (*m_fnRead32)(ulPciAddress, 0x0000);
+				if ((ulDeviceVendor == DEVVEND(ROMEO_VENDORID, ROMEO_DEVICEID)) || (ulDeviceVendor == DEVVEND(ROMEO_VENDORID, ROMEO_DEVICEID2)))
+				{
+					return ulPciAddress;
 				}
 			}
 		}
 	}
-	return((ULONG)0xffffffff);
+	return static_cast<ULONG>(-1);
 }
 
+/**
+ * ROMEO は有効?
+ * @retval true 有効
+ * @retval false 無効
+ */
+bool CJuliet::IsEnabled() const
+{
+	return (m_hModule != NULL);
+}
 
+/**
+ * ビジー?
+ * @retval true ビジー
+ * @retval false レディ
+ */
+bool CJuliet::IsBusy() const
+{
+	return (!m_bOpna) || (((*m_fnIn8)(m_ulAddress + ROMEO_YMF288ADDR1) & 0x80) != 0);
 
-BOOL juliet_initialize(void) {
+}
 
-	HMODULE		mod;
-const DLLPROC	*d;
-const DLLPROC	*dterm;
-	long		proc;
-	ULONG		pciaddr;
+/**
+ * 音源リセット
+ */
+void CJuliet::Reset() const
+{
+	if (m_bOpna)
+	{
+		(*m_fnOut32)(m_ulAddress + ROMEO_YMF288CTRL, 0x00);
+		::Sleep(150);
 
-	juliet_deinitialize();
-
-	mod = LoadLibrary(PCIDEBUG_DLL);
-	if (mod == NULL) {
-		goto jini_err1;
+		(*m_fnOut32)(m_ulAddress + ROMEO_YMF288CTRL, 0x80);
+		::Sleep(150);
 	}
-	romeo.mod = mod;
-	d = dllproc;
-	dterm = d + NELEMENTS(dllproc);
-	while(d < dterm) {
-		proc = (long)GetProcAddress(mod, d->symbol);
-		if (proc == (long)NULL) {
-			goto jini_err2;
+}
+
+/**
+ * レジスタ書き込み
+ * @param[in] nAddr アドレス
+ * @param[in] cData データ
+ */
+void CJuliet::WriteRegister(UINT nAddr, UINT8 cData)
+{
+	if (m_bOpna)
+	{
+		if (nAddr == 0x07)
+		{
+			// psg mix
+			m_cPsgMix = cData;
 		}
-		*(long *)(((UINT8 *)&romeo) + (d->addr)) = proc;
-		d++;
+		else if ((nAddr & (~15)) == 0x40)
+		{
+			// ttl
+			m_cTtl[nAddr & 15] = cData;
+		}
+		else if ((nAddr & (~3)) == 0xb0)
+		{
+			// algorithm
+			m_cAlgorithm[nAddr & 3] = cData;
+		}
+		WriteRegisterInner(nAddr, cData);
 	}
-
-	pciaddr = searchRomeo();
-	if (pciaddr == (ULONG)0xffffffff) {
-		goto jini_err2;
-	}
-	romeo.addr = romeo.read32(pciaddr, ROMEO_BASEADDRESS1);
-	romeo.irq  = romeo.read32(pciaddr, ROMEO_PCIINTERRUPT) & 0xff;
-	if (!romeo.addr) {
-		goto jini_err2;
-	}
-	romeo.avail = ROMEO_AVAIL | ROMEO_YMF288;
-	juliet_YMF288Reset();
-	TRACEOUT(("ROMEO enable"));
-	return(SUCCESS);
-
-jini_err2:
-	juliet_deinitialize();
-
-jini_err1:
-	return(FAILURE);
 }
 
-void juliet_deinitialize(void) {
-
-	if (romeo.mod) {
-		FreeLibrary(romeo.mod);
+/**
+ * 拡張レジスタ書き込み
+ * @param[in] nAddr アドレス
+ * @param[in] cData データ
+ */
+void CJuliet::WriteExtendRegister(UINT nAddr, UINT8 cData)
+{
+	if (m_bOpna)
+	{
+		if ((nAddr & (~15)) == 0x40)
+		{
+			// ttl
+			m_cTtl[0x10 + (nAddr & 15)] = cData;
+		}
+		else if ((nAddr & (~3)) == 0xb0)
+		{
+			// algorithm
+			m_cAlgorithm[4 + (nAddr & 3)] = cData;
+		}
+		WriteExtendRegisterInner(nAddr, cData);
 	}
-	ZeroMemory(&romeo, sizeof(romeo));
-	FillMemory(romeo.ttl, 8*4, 0x7f);
-	romeo.psgmix = 0x3f;
 }
 
+/**
+ * ミュート
+ * @param[in] bMute ミュート
+ */
+void CJuliet::Mute(bool bMute) const
+{
+	if (m_bOpna)
+	{
+		WriteRegisterInner(0x07, (bMute) ? 0x3f : m_cPsgMix);
 
-// ---- YMF288部
-
-static void YMF288A(UINT8 addr, UINT8 data) {
-
-	while(romeo.in8(romeo.addr + ROMEO_YMF288ADDR1) & 0x80) {
-		Sleep(0);
+		const int nVolume = (bMute) ? -127 : 0;
+		for (UINT ch = 0; ch < 3; ch++)
+		{
+			SetVolume(ch + 0, nVolume);
+			SetVolume(ch + 4, nVolume);
+		}
 	}
-	romeo.out8(romeo.addr + ROMEO_YMF288ADDR1, addr);
-	while(romeo.in8(romeo.addr + ROMEO_YMF288ADDR1) & 0x80) {
-		Sleep(0);
-	}
-	romeo.out8(romeo.addr + ROMEO_YMF288DATA1, data);
 }
 
-static void YMF288B(UINT8 addr, UINT8 data) {
+/**
+ * レジスタ書き込み(内部)
+ * @param[in] cAddr アドレス
+ * @param[in] cData データ
+ */
+void CJuliet::WriteRegisterInner(UINT8 cAddr, UINT8 cData) const
+{
+	while (((*m_fnIn8)(m_ulAddress + ROMEO_YMF288ADDR1) & 0x80) != 0)
+	{
+		::Sleep(0);
+	}
+	(*m_fnOut8)(m_ulAddress + ROMEO_YMF288ADDR1, cAddr);
 
-	while(romeo.in8(romeo.addr + ROMEO_YMF288ADDR1) & 0x80) {
-		Sleep(0);
+	while (((*m_fnIn8)(m_ulAddress + ROMEO_YMF288ADDR1) & 0x80) != 0)
+	{
+		::Sleep(0);
 	}
-	romeo.out8(romeo.addr + ROMEO_YMF288ADDR2, addr);
-	while(romeo.in8(romeo.addr + ROMEO_YMF288ADDR1) & 0x80) {
-		Sleep(0);
-	}
-	romeo.out8(romeo.addr + ROMEO_YMF288DATA2, data);
+	(*m_fnOut8)(m_ulAddress + ROMEO_YMF288DATA1, cData);
 }
 
-static void setvolume(UINT8 ch, int vol) {
+/**
+ * 拡張レジスタ書き込み(内部)
+ * @param[in] cAddr アドレス
+ * @param[in] cData データ
+ */
+void CJuliet::WriteExtendRegisterInner(UINT8 cAddr, UINT8 cData) const
+{
+	while (((*m_fnIn8)(m_ulAddress + ROMEO_YMF288ADDR1) & 0x80) != 0)
+	{
+		::Sleep(0);
+	}
+	(*m_fnOut8)(m_ulAddress + ROMEO_YMF288ADDR2, cAddr);
 
-	void	(*send)(UINT8 addr, UINT8 data);
-	UINT8	mask;
-	UINT8	*pttl;
-	int		ttl;
+	while (((*m_fnIn8)(m_ulAddress + ROMEO_YMF288ADDR1) & 0x80) != 0)
+	{
+		::Sleep(0);
+	}
+	(*m_fnOut8)(m_ulAddress + ROMEO_YMF288DATA2, cData);
+}
 
-	send = (ch & 4)?YMF288B:YMF288A;
-	mask = opmask[romeo.algo[ch & 7] & 7];
-	pttl = romeo.ttl + ((ch & 4) << 2);
-	ch = 0x40 + (ch & 3);
-	do {
-		if (mask & 1) {
-			ttl = pttl[ch & 0x0f] & 0x7f;
-			ttl -= vol;
-			if (ttl < 0) {
-				ttl = 0;
+/**
+ * ヴォリューム設定
+ * @param[in] nChannel チャンネル
+ * @param[in] nVolume ヴォリューム値
+ */
+void CJuliet::SetVolume(UINT nChannel, int nVolume) const
+{
+	// 書き込み関数
+	void (CJuliet::*fnSetReg)(UINT8 cAddr, UINT8 cData) const = (nChannel & 4) ? &CJuliet::WriteExtendRegisterInner : &CJuliet::WriteRegisterInner;
+
+	UINT8 cMask = s_opmask[m_cAlgorithm[nChannel & 7] & 7];
+	const UINT8* pTtl = m_cTtl + ((nChannel & 4) << 2);
+
+	int nOffset = nChannel & 3;
+	do
+	{
+		if (cMask & 1)
+		{
+			int nTtl = (pTtl[nOffset] & 0x7f) - nVolume;
+			if (nTtl < 0)
+			{
+				nTtl = 0;
 			}
-			else if (ttl > 0x7f) {
-				ttl = 0x7f;
+			else if (nTtl > 0x7f)
+			{
+				nTtl = 0x7f;
 			}
-			send(ch, (UINT8)ttl);
+			(this->*fnSetReg)(0x40 + nOffset, static_cast<UINT8>(nTtl));
 		}
-		ch += 4;
-		mask >>= 1;
-	} while(mask);
+		nOffset += 4;
+		cMask >>= 1;
+	} while (cMask != 0);
 }
 
 
-void juliet_YMF288Reset(void) {
+// ----
 
-	if (romeo.avail & ROMEO_YMF288) {
-		romeo.out32(romeo.addr + ROMEO_YMF288CTRL, 0x00);
-		Sleep(150);
-		romeo.out32(romeo.addr + ROMEO_YMF288CTRL, 0x80);
-		Sleep(150);
-	}
+static CJuliet romeo;
+
+BOOL juliet_initialize(void)
+{
+	return romeo.Initialize() ? SUCCESS : FAILURE;
 }
 
-BOOL juliet_YMF288IsEnable(void) {
-
-	return((romeo.avail & ROMEO_YMF288) != 0);
+void juliet_deinitialize(void)
+{
+	romeo.Deinitialize();
 }
 
-BOOL juliet_YMF288IsBusy(void) {
-
-	return((!(romeo.avail & ROMEO_YMF288)) ||
-			((romeo.in8(romeo.addr + ROMEO_YMF288ADDR1) & 0x80) != 0));
+void juliet_YMF288Reset(void)
+{
+	romeo.Reset();
 }
 
-void juliet_YMF288A(UINT addr, UINT8 data) {
-
-	if (romeo.avail & ROMEO_YMF288) {
-		if (addr == 0x07) {							// psg mix
-			romeo.psgmix = data;
-		}
-		else if ((addr & (~15)) == 0x40) {			// ttl
-			romeo.ttl[addr & 15] = data;
-		}
-		else if ((addr & (~3)) == 0xb0) {			// algorithm
-			romeo.algo[addr & 3] = data;
-		}
-		YMF288A(addr, data);
-	}
+BOOL juliet_YMF288IsEnable(void)
+{
+	return romeo.IsEnabled();
 }
 
-void juliet_YMF288B(UINT addr, UINT8 data) {
-
-	if (romeo.avail & ROMEO_YMF288) {
-		if ((addr & (~15)) == 0x40) {				// ttl
-			romeo.ttl[0x10 + (addr & 15)] = data;
-		}
-		else if ((addr & (~3)) == 0xb0) {			// algorithm
-			romeo.algo[4 + (addr & 3)] = data;
-		}
-		YMF288B(addr, data);
-	}
+BOOL juliet_YMF288IsBusy(void)
+{
+	return romeo.IsBusy();
 }
 
-void juliet_YMF288Enable(BOOL enable) {
+void juliet_YMF288A(UINT addr, UINT8 data)
+{
+	romeo.WriteRegister(addr, data);
+}
 
-	UINT8	ch;
-	int		vol;
+void juliet_YMF288B(UINT addr, UINT8 data)
+{
+	romeo.WriteExtendRegister(addr, data);
+}
 
-	if (romeo.avail & ROMEO_YMF288) {
-		YMF288A(0x07, (enable)?romeo.psgmix:0x3f);
-		vol = (enable)?0:-127;
-		for (ch=0; ch<3; ch++) {
-			setvolume(ch+0, vol);
-			setvolume(ch+4, vol);
-		}
-	}
+void juliet_YMF288Enable(BOOL enable)
+{
+	romeo.Mute(!enable);
 }
 
 #endif
-
