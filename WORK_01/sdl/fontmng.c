57e26a61fa51a5cf0ac74d71c7657b9ea98ed3e1
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2014-04-19 06:51:58 +0000

    ✓ 文字コード (互換性トル)
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1474 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- WORK_01/sdl/fontmng.c
+++ WORK_01/sdl/fontmng.c
@@ -217,10 +217,12 @@ static void getlength1(FNTMNG fhdl, FNTDAT fdat,
 											const char *string, int length) {
 
 	UINT16		utext[2];
+	int			r;
 	SDL_Surface	*text;
 
 	if (fhdl->fonttype & FDAT_PROPORTIONAL) {
-		codecnv_euctoucs2(utext, NELEMENTS(utext), string, length);
+		r = codecnv_euctoucs2(utext, 1, string, length);
+		utext[1] = 0;
 		text = TTF_RenderUNICODE_Solid(fhdl->ttf_font, utext, white);
 		setfdathead(fhdl, fdat, length, text);
 		if (text) {
