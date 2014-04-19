950dc7432dcf09085fa8a7a6a9664a8e0fde572a
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2014-04-19 10:07:50 +0000

    ✓ 文字コード: UTF-8
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1478 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- WORK_01/codecnv/sjisucs2.c
+++ WORK_01/codecnv/sjisucs2.c
@@ -1154,6 +1154,7 @@ static const UINT32 s_level1[] =
 };
 
 static UINT sjis2ucs2(UINT16 *lpOutput, UINT cchOutput, const char *lpInput, UINT cchInput);
+static UINT sjis2utf8(char *lpOutput, UINT cchOutput, const char *lpInput, UINT cchInput);
 
 /**
  * Maps a S-JIS string to a UTF-16 string
@@ -1237,6 +1238,124 @@ static UINT sjis2ucs2(UINT16 *lpOutput, UINT cchOutput, const char *lpInput, UIN
 		if (lpOutput)
 		{
 			*lpOutput++ = (UINT16)r;
+		}
+	}
+	return (UINT)(cchOutput - nRemain);
+}
+
+/**
+ * Maps a S-JIS string to a UTF-8 string
+ * @param[out] lpOutput Pointer to a buffer that receives the converted string
+ * @param[in] cchOutput Size, in characters, of the buffer indicated by lpOutput
+ * @param[in] lpInput Pointer to the character string to convert
+ * @param[in] cchInput Size, in characters, of the buffer indicated by lpInput
+ * @return The number of characters written to the buffer indicated by lpOutput
+ */
+UINT codecnv_sjistoutf8(char *lpOutput, UINT cchOutput, const char *lpInput, UINT cchInput)
+{
+	UINT nLength;
+
+	if (lpInput == NULL)
+	{
+		return 0;
+	}
+
+	if (cchOutput == 0)
+	{
+		lpOutput = NULL;
+		cchOutput = (UINT)-1;
+	}
+
+	if (cchInput != (UINT)-1)
+	{
+		// Binary mode
+		return sjis2utf8(lpOutput, cchOutput, lpInput, cchInput);
+	}
+	else
+	{
+		// String mode
+		nLength = sjis2utf8(lpOutput, cchOutput - 1, lpInput, strlen(lpInput));
+		if (lpOutput)
+		{
+			lpOutput[nLength] = '\0';
+		}
+		return nLength + 1;
+	}
+}
+
+/**
+ * Maps a S-JIS string to a UTF-8 string (inner)
+ * @param[out] lpOutput Pointer to a buffer that receives the converted string
+ * @param[in] cchOutput Size, in characters, of the buffer indicated by lpOutput
+ * @param[in] lpInput Pointer to the character string to convert
+ * @param[in] cchInput Size, in characters, of the buffer indicated by lpInput
+ * @return The number of characters written to the buffer indicated by lpOutput
+ */
+static UINT sjis2utf8(char *lpOutput, UINT cchOutput, const char *lpInput, UINT cchInput)
+{
+	UINT nRemain;
+	UINT c;
+	UINT r;
+
+	nRemain = cchOutput;
+	while ((cchInput > 0) && (nRemain > 0))
+	{
+		c = (*lpInput++) & 0xff;
+		cchInput--;
+
+		r = s_level1[c];
+		if (r & 0xffff0000)
+		{
+			if (cchInput == 0)
+			{
+				break;
+			}
+			cchInput--;
+			c = ((*lpInput++) - r) & 0xff;
+			if (c < ((r >> 8) & 0xff))
+			{
+				r = s_level2[((r >> 16) & 0xffff) + c];
+			}
+			else
+			{
+				r = UDCODE;
+			}
+		}
+
+		if (r < 0x80)
+		{
+			nRemain--;
+			if (lpOutput)
+			{
+				*lpOutput++ = (char)r;
+			}
+		}
+		else if (r < 0x800)
+		{
+			if (nRemain < 2)
+			{
+				break;
+			}
+			nRemain -= 2;
+			if (lpOutput)
+			{
+				*lpOutput++ = (char)(0xc0 + ((r >> 6) & 0x1f));
+				*lpOutput++ = (char)(0x80 + ((r >> 0) & 0x3f));
+			}
+		}
+		else
+		{
+			if (nRemain < 3)
+			{
+				break;
+			}
+			nRemain -= 3;
+			if (lpOutput)
+			{
+				*lpOutput++ = (char)(0xe0 + ((r >> 12) & 0x0f));
+				*lpOutput++ = (char)(0x80 + ((r >> 6) & 0x3f));
+				*lpOutput++ = (char)(0x80 + ((r >> 0) & 0x3f));
+			}
 		}
 	}
 	return (UINT)(cchOutput - nRemain);
