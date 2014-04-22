da02ba405fc06c9b9f15ea6075bd92c481071b24
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2014-04-22 01:35:14 +0000

    ✓ SDL2 Mac: プリコンパイル済ヘッダを利用する
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1506 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- WORK_01/sdl2/MacOSX/np2sdl2/compiler.h
+++ WORK_01/sdl2/MacOSX/np2sdl2/compiler.h
@@ -1,3 +1,12 @@
+/**
+ * @file	compiler.h
+ * @brief	include file for standard system include files,
+ *			or project specific include files that are used frequently,
+ *			but are changed infrequently
+ */
+
+#pragma once
+
 #include <stdio.h>
 #include <stddef.h>
 #include <SDL2/SDL.h>
