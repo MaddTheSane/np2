1b1731f51a79e729b826f05f3a4a255106a76f14
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2014-06-14 07:37:51 +0000

    ✓ SDL2: updates cfg immediately  svn merge -r 135:136 https://amethyst.yui.ne.jp/svn-dev/xmil-pre/branches/yui/WORK_01
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1588 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- WORK_01/sdl2/sysmng.c
+++ WORK_01/sdl2/sysmng.c
@@ -1,6 +1,41 @@
-#include	"compiler.h"
-#include	"sysmng.h"
+/**
+ *	@file	sysmng.c
+ *	@brief	Implementation of the system
+ */
 
+#include "compiler.h"
+#include "sysmng.h"
+#include "ini.h"
 
-	UINT	sys_updates;
+/**
+ * Initialize
+ */
+void sysmng_initialize(void)
+{
+}
 
+/**
+ * Deinitialize
+ */
+void sysmng_deinitialize(void)
+{
+}
+
+/**
+ * Notifies flags
+ * @param[in] update update flags
+ */
+void sysmng_update(UINT update)
+{
+	if (update & (SYS_UPDATECFG | SYS_UPDATEOSCFG))
+	{
+		initsave();
+	}
+}
+
+/**
+ * Notifies CPU Reset
+ */
+void sysmng_cpureset(void)
+{
+}
