6d216b77205eb60d76ee557446854f441c75fe76
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2014-06-26 11:47:32 +0000

    ✓ MacOSX: 10.7 で起動しない
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1594 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- WORK_01/sdl2/MacOSX/np2sdl2/main.m
+++ WORK_01/sdl2/MacOSX/np2sdl2/main.m
@@ -18,5 +18,15 @@ int main(int argc, const char * argv[])
 	NSString *pstrBundlePath = [[NSBundle mainBundle] bundlePath];
 	file_setcd([pstrBundlePath UTF8String]);
 
+	for (int i = 1; i < argc; i++)
+	{
+		if (strncmp(argv[i], "-psn_", 5) == 0)
+		{
+			argc--;
+			memmove(argv + i, argv + i + 1, (argc - i + 1) * sizeof(argv[0]));
+			break;
+		}
+	}
+
 	return np2_main(argc, argv);
 }
