de54cdde130768fe2471be04f9dcf58f6c498332
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2015-07-24 10:26:21 +0000

    ✓ trace
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1925 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- reform_ui/win9x/trace.cpp
+++ reform_ui/win9x/trace.cpp
@@ -15,7 +15,6 @@
 
 #ifdef TRACE
 
-#define	VIEW_BUFFERSIZE	4096
 #define	VIEW_FGCOLOR	0x000000
 #define	VIEW_BGCOLOR	0xffffff
 #define	VIEW_TEXT		"ＭＳ ゴシック"
@@ -50,14 +49,6 @@ private:
 	HBRUSH m_hBrush;					/*!< ブラシ */
 	HFONT m_hFont;						/*!< フォント */
 	CWndProc m_wndView;					/*!< テキスト コントロール */
-
-private:
-	int viewpos;
-	int viewleng;
-	TCHAR viewbuf[VIEW_BUFFERSIZE * 2];
-	void View_ScrollToBottom();
-	void View_ClrString();
-	void View_AddString(const OEMCHAR *string);
 };
 
 struct TRACECFG
@@ -88,68 +79,7 @@ static const PFTBL initbl[4] =
 	PFVAL("height",	PFTYPE_SINT32,	&tracecfg.height)
 };
 
-
-// ---- View
-
-void CTraceWnd::View_ScrollToBottom()
-{
-	int MinPos;
-	int MaxPos;
-
-	GetScrollRange(m_wndView, SB_VERT, &MinPos, &MaxPos);
-	m_wndView.PostMessage(EM_LINESCROLL, 0, MaxPos);
-}
-
-void CTraceWnd::View_ClrString(void)
-{
-	viewpos = 0;
-	viewleng = 0;
-	viewbuf[0] = '\0';
-	m_wndView.SetWindowText(viewbuf);
-}
-
-void CTraceWnd::View_AddString(const OEMCHAR *string)
-{
-	int		slen;
-	int		vpos;
-	int		vlen;
-	TCHAR	c;
-
-	slen = lstrlen(string);
-	if ((slen == 0) || ((slen + 3) > VIEW_BUFFERSIZE)) {
-		return;
-	}
-	vpos = viewpos;
-	vlen = viewleng;
-	if ((vpos + vlen + slen + 3) > (VIEW_BUFFERSIZE * 2)) {
-		while(vlen > 0) {
-			vlen--;
-			c = viewbuf[vpos++];
-			if ((c == 0x0a) && ((vlen + slen + 3) <= VIEW_BUFFERSIZE)) {
-				break;
-			}
-		}
-		if (vpos >= VIEW_BUFFERSIZE) {
-			if (vlen) {
-				CopyMemory(viewbuf, viewbuf + vpos, vlen * sizeof(TCHAR));
-			}
-			vpos = 0;
-			viewpos = 0;
-		}
-	}
-	CopyMemory(viewbuf + vpos + vlen, string, slen * sizeof(TCHAR));
-	vlen += slen;
-	viewbuf[vpos + vlen + 0] = '\r';
-	viewbuf[vpos + vlen + 1] = '\n';
-	viewbuf[vpos + vlen + 2] = '\0';
-	viewleng = vlen + 2;
-	m_wndView.SetWindowText(viewbuf + vpos);
-	View_ScrollToBottom();
-}
-
-
-// ----
-
+//! 唯一のインスタンスです
 CTraceWnd CTraceWnd::sm_instance;
 
 /**
@@ -170,6 +100,60 @@ CTraceWnd::CTraceWnd()
 	, m_hBrush(NULL)
 	, m_hFont(NULL)
 {
+}
+
+/**
+ * 初期化
+ */
+void CTraceWnd::Initialize()
+{
+	WNDCLASS wc;
+	ZeroMemory(&wc, sizeof(wc));
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = ::DefWindowProc;
+	wc.hInstance = GetInstanceHandle();
+	wc.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));
+	wc.lpszClassName = s_szClassName;
+	if (!::RegisterClass(&wc))
+	{
+		return;
+	}
+
+#if 1
+	m_nFlags = 4;
+#else
+	m_nFlags = 1;
+	m_tfh = textfile_create(OEMTEXT("traceout.txt"), 0x800);
+#endif
+
+	tracecfg.posx = CW_USEDEFAULT;
+	tracecfg.posy = CW_USEDEFAULT;
+	tracecfg.width = CW_USEDEFAULT;
+	tracecfg.height = CW_USEDEFAULT;
+	ini_read(file_getcd(np2trace), inititle, initbl, NELEMENTS(initbl));
+
+	if (!CreateEx(WS_EX_CONTROLPARENT, s_szClassName, s_szTitle, WS_OVERLAPPEDWINDOW, tracecfg.posx, tracecfg.posy, tracecfg.width, tracecfg.height, NULL, NULL))
+	{
+		return;
+	}
+	ShowWindow(SW_SHOW);
+	UpdateWindow();
+}
+
+/**
+ * 解放
+ */
+void CTraceWnd::Deinitialize()
+{
+	if (m_tfh != NULL)
+	{
+		textfile_close(m_tfh);
+		m_tfh = NULL;
+	}
+
+	DestroyWindow();
+	ini_write(file_getcd(np2trace), inititle, initbl, NELEMENTS(initbl));
 }
 
 /**
@@ -210,81 +194,21 @@ void CTraceWnd::AddString(LPCTSTR lpString)
 {
 	if ((m_nFlags & 4) && (m_wndView.IsWindow()))
 	{
-		View_AddString(lpString);
+		UINT nStart = 0;
+		UINT nEnd = 0;
+		m_wndView.SendMessage(EM_GETSEL, reinterpret_cast<WPARAM>(&nStart), reinterpret_cast<LPARAM>(&nEnd));
+
+		const UINT nLength = m_wndView.GetWindowTextLength();
+		m_wndView.SendMessage(EM_SETSEL, static_cast<WPARAM>(nLength), static_cast<LPARAM>(nLength));
+
+		m_wndView.SendMessage(EM_REPLACESEL, FALSE, reinterpret_cast<LPARAM>(lpString));
+		m_wndView.SendMessage(EM_REPLACESEL, FALSE, reinterpret_cast<LPARAM>(crlf));
+		m_wndView.SendMessage(EM_SETSEL, static_cast<WPARAM>(nStart), static_cast<LPARAM>(nEnd));
 	}
 	if (m_tfh != NULL)
 	{
 		textfile_write(m_tfh, lpString);
 		textfile_write(m_tfh, crlf);
-	}
-}
-
-/**
- * フレームワークは、Windows のウィンドウは [作成] または CreateEx のメンバー関数を呼び出すことによって作成されたアプリケーションが必要とすると、このメンバー関数を呼び出します
- * @param[in] lpCreateStruct 作成されたオブジェクトに関する情報が含まれています。
- * @retval 0 成功
- * @retval -1 失敗
- */
-int CTraceWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
-{
-	HMENU hMenu = GetSystemMenu(FALSE);
-	menu_addmenures(hMenu, 0, IDR_TRACE, FALSE);
-
-	m_hBrush = ::CreateSolidBrush(VIEW_BGCOLOR);
-
-	RECT rc;
-	GetClientRect(&rc);
-	if (m_wndView.CreateEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), NULL, WS_CHILD | WS_VISIBLE | ES_READONLY | ES_LEFT | ES_MULTILINE | WS_VSCROLL | ES_AUTOVSCROLL, 0, 0, rc.right, rc.bottom, m_hWnd, NULL))
-	{
-		m_wndView.SendMessage(EM_SETLIMITTEXT, VIEW_BUFFERSIZE, 0);
-
-		m_hFont = ::CreateFont(VIEW_SIZE, 0, 0, 0, 0, 0, 0, 0,  SHIFTJIS_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, FIXED_PITCH, s_szViewFont);
-		if (m_hFont)
-		{
-			m_wndView.SetFont(m_hFont);
-		}
-		m_wndView.SetFocus();
-	}
-
-	return 0;
-}
-
-/**
- * フレームワークは、ユーザーがコントロール メニューからコマンドを選択したとき、または最大化または最小化ボタンを選択すると、このメンバー関数を呼び出します
- * @param[in] nID 必要なシステム コマンドの種類を指定します
- * @param[in] lParam カーソルの座標
- */
-void CTraceWnd::OnSysCommand(UINT nID, LPARAM lParam)
-{
-	switch (nID)
-	{
-		case IDM_TRACE_TRACE:
-			m_nFlags ^= 1;
-			break;
-
-		case IDM_TRACE_VERBOSE:
-			m_nFlags ^= 2;
-			break;
-
-		case IDM_TRACE_ENABLE:
-			m_nFlags ^= 4;
-			break;
-
-		case IDM_TRACE_FILEOUT:
-			if (m_tfh != NULL)
-			{
-				textfile_close(m_tfh);
-				m_tfh = NULL;
-			}
-			else
-			{
-				m_tfh = textfile_create(OEMTEXT("traceout.txt"), 0x800);
-			}
-			break;
-
-		case IDM_TRACE_CLEAR:
-			View_ClrString();
-			break;
 	}
 }
 
@@ -329,7 +253,6 @@ LRESULT CTraceWnd::WindowProc(UINT nMsg, WPARAM wParam, LPARAM lParam)
 				tracecfg.height = rc.bottom - rc.top;
 			}
 			m_wndView.MoveWindow(0, 0, LOWORD(lParam), HIWORD(lParam));
-			View_ScrollToBottom();
 			break;
 
 		case WM_SETFOCUS:
@@ -376,6 +299,73 @@ LRESULT CTraceWnd::WindowProc(UINT nMsg, WPARAM wParam, LPARAM lParam)
 }
 
 /**
+ * フレームワークは、Windows のウィンドウは [作成] または CreateEx のメンバー関数を呼び出すことによって作成されたアプリケーションが必要とすると、このメンバー関数を呼び出します
+ * @param[in] lpCreateStruct 作成されたオブジェクトに関する情報が含まれています。
+ * @retval 0 成功
+ * @retval -1 失敗
+ */
+int CTraceWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	HMENU hMenu = GetSystemMenu(FALSE);
+	menu_addmenures(hMenu, 0, IDR_TRACE, FALSE);
+
+	m_hBrush = ::CreateSolidBrush(VIEW_BGCOLOR);
+
+	RECT rc;
+	GetClientRect(&rc);
+	if (m_wndView.CreateEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), NULL, WS_CHILD | WS_VISIBLE | ES_READONLY | ES_LEFT | ES_MULTILINE | WS_VSCROLL | ES_AUTOVSCROLL, 0, 0, rc.right, rc.bottom, m_hWnd, NULL))
+	{
+		m_hFont = ::CreateFont(VIEW_SIZE, 0, 0, 0, 0, 0, 0, 0,  SHIFTJIS_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, FIXED_PITCH, s_szViewFont);
+		if (m_hFont)
+		{
+			m_wndView.SetFont(m_hFont);
+		}
+		m_wndView.SetFocus();
+	}
+
+	return 0;
+}
+
+/**
+ * フレームワークは、ユーザーがコントロール メニューからコマンドを選択したとき、または最大化または最小化ボタンを選択すると、このメンバー関数を呼び出します
+ * @param[in] nID 必要なシステム コマンドの種類を指定します
+ * @param[in] lParam カーソルの座標
+ */
+void CTraceWnd::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	switch (nID)
+	{
+		case IDM_TRACE_TRACE:
+			m_nFlags ^= 1;
+			break;
+
+		case IDM_TRACE_VERBOSE:
+			m_nFlags ^= 2;
+			break;
+
+		case IDM_TRACE_ENABLE:
+			m_nFlags ^= 4;
+			break;
+
+		case IDM_TRACE_FILEOUT:
+			if (m_tfh != NULL)
+			{
+				textfile_close(m_tfh);
+				m_tfh = NULL;
+			}
+			else
+			{
+				m_tfh = textfile_create(OEMTEXT("traceout.txt"), 0x800);
+			}
+			break;
+
+		case IDM_TRACE_CLEAR:
+			m_wndView.SetWindowText(TEXT(""));
+			break;
+	}
+}
+
+/**
  * フレームワークは、メニュー ループ開始時に、このメンバー関数を呼び出します
  * @param[in] bIsTrackPopupMenu TrackPopupMenu 関数を利用した場合 TRUE
  */
@@ -397,63 +387,9 @@ void trace_init(void)
 	CTraceWnd::GetInstance()->Initialize();
 }
 
-/**
- * 初期化
- */
-void CTraceWnd::Initialize()
-{
-	WNDCLASS wc;
-	ZeroMemory(&wc, sizeof(wc));
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = ::DefWindowProc;
-	wc.hInstance = GetInstanceHandle();
-	wc.hCursor = ::LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));
-	wc.lpszClassName = s_szClassName;
-	if (!::RegisterClass(&wc))
-	{
-		return;
-	}
-
-#if 1
-	m_nFlags = 4;
-#else
-	m_nFlags = 1;
-	m_tfh = textfile_create(OEMTEXT("traceout.txt"), 0x800);
-#endif
-
-	tracecfg.posx = CW_USEDEFAULT;
-	tracecfg.posy = CW_USEDEFAULT;
-	tracecfg.width = CW_USEDEFAULT;
-	tracecfg.height = CW_USEDEFAULT;
-	ini_read(file_getcd(np2trace), inititle, initbl, NELEMENTS(initbl));
-
-	if (!CreateEx(WS_EX_CONTROLPARENT, s_szClassName, s_szTitle, WS_OVERLAPPEDWINDOW, tracecfg.posx, tracecfg.posy, tracecfg.width, tracecfg.height, NULL, NULL))
-	{
-		return;
-	}
-	ShowWindow(SW_SHOW);
-	UpdateWindow();
-}
-
 void trace_term(void)
 {
 	CTraceWnd::GetInstance()->Deinitialize();
-}
-
-/**
- * 解放
- */
-void CTraceWnd::Deinitialize()
-{
-	if (m_tfh != NULL)
-	{
-		textfile_close(m_tfh);
-		m_tfh = NULL;
-	}
-
-	DestroyWindow();
-	ini_write(file_getcd(np2trace), inititle, initbl, NELEMENTS(initbl));
 }
 
 void trace_fmt(const char *fmt, ...)
