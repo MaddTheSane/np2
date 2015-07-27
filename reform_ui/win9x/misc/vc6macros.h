f8be0d1e728b31a0fc83d9b560b663f78fb7361b
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2015-07-27 07:32:41 +0000

    ✓ moves defines　(cont.)
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1931 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- reform_ui/win9x/misc/vc6macros.h
+++ reform_ui/win9x/misc/vc6macros.h
@@ -13,7 +13,6 @@
 #if (_MSC_VER < 1300)
 //! for scope
 #define for					if (0 /*NEVER*/) { /* no process */ } else for
-
 #endif	// (_MSC_VER < 1300)
 
 #ifndef GET_X_LPARAM
@@ -23,4 +22,35 @@
 #ifndef GET_Y_LPARAM
 //! y-coordinate from LPARAM
 #define GET_Y_LPARAM(lp)	((int)(short)HIWORD(lp))
+#endif
+
+// for VC6SDK
+#if (_MSC_VER < 1300)
+#ifndef LONG_PTR
+#define LONG_PTR			LONG
+#endif
+#ifndef DWORD_PTR
+#define DWORD_PTR			DWORD
+#endif
+#ifndef GetWindowLongPtr
+#define GetWindowLongPtr	GetWindowLong
+#endif
+#ifndef SetWindowLongPtr
+#define SetWindowLongPtr	SetWindowLong
+#endif
+#ifndef GWLP_WNDPROC
+#define GWLP_WNDPROC		GWL_WNDPROC
+#endif
+#ifndef GWLP_HINSTANCE
+#define GWLP_HINSTANCE		GWL_HINSTANCE
+#endif
+#ifndef GWLP_HWNDPARENT
+#define GWLP_HWNDPARENT		GWL_HWNDPARENT
+#endif
+#ifndef GWLP_USERDATA
+#define GWLP_USERDATA		GWL_USERDATA
+#endif
+#ifndef GWLP_ID
+#define GWLP_ID				GWL_ID
+#endif
 #endif
