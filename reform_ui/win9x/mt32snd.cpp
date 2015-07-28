97257214ebfb7b3fc055a4b8b55f18860cb64f1e
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2015-07-28 12:58:19 +0000

    ✓ mt32
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@1945 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- reform_ui/win9x/mt32snd.cpp
+++ reform_ui/win9x/mt32snd.cpp
@@ -1,130 +1,147 @@
-#include	"compiler.h"
+#include "compiler.h"
 
 #if defined(MT32SOUND_DLL)
 
-#include	"mt32snd.h"
+#include "mt32snd.h"
 
-#define	MT32SAMPLES		512
+MT32Sound MT32Sound::sm_instance;
 
+// DLL名
+static const TCHAR s_szMT32SoundDll[] = TEXT("mt32sound.dll");
 
-typedef struct {
-	HMODULE	mod;
-	BOOL	opened;
-	UINT	rate;
-	int		(WINAPI * Open)(int rate, int reverb, int def, int revtype, int revtime, int revlvl);
-	int		(WINAPI * Close)(void);
-	int		(WINAPI * Write)(unsigned char data);
-	int		(WINAPI * Mix)(void *buff, unsigned long size);
-} MT32SOUND;
-
-static	MT32SOUND	mt32sound;
-
-
-typedef struct {
-const char	*symbol;
-	UINT	addr;
-} DLLPROC;
-
-static const TCHAR mt32sounddll[] = _T("mt32sound.dll");
 static const char fn_mt32soundopen[] = "MT32Sound_Open";
 static const char fn_mt32soundclose[] = "MT32Sound_Close";
 static const char fn_mt32soundwrite[] = "MT32Sound_Write";
 static const char fn_mt32soundmix[] = "MT32Sound_Mix";
 
-static const DLLPROC dllproc[] = {
-				{fn_mt32soundopen,	offsetof(MT32SOUND, Open)},
-				{fn_mt32soundclose,	offsetof(MT32SOUND, Close)},
-				{fn_mt32soundwrite,	offsetof(MT32SOUND, Write)},
-				{fn_mt32soundmix,	offsetof(MT32SOUND, Mix)}};
+/**
+ * コンストラクタ
+ */
+MT32Sound::MT32Sound()
+	: m_hModule(NULL)
+	, m_bOpened(false)
+	, m_nRate(0)
+	, m_fnOpen(NULL)
+	, m_fnClose(NULL)
+	, m_fnWrite(NULL)
+	, m_fnMix(NULL)
+{
+}
 
+/**
+ * デストラクタ
+ */
+MT32Sound::~MT32Sound()
+{
+	Deinitialize();
+}
 
-BRESULT mt32sound_initialize(void) {
+/**
+ * 初期化
+ * @retval true 成功
+ * @retval false 失敗
+ */
+bool MT32Sound::Initialize()
+{
+	Deinitialize();
 
-	HMODULE		mod;
-const DLLPROC	*d;
-const DLLPROC	*dterm;
-	FARPROC		proc;
-
-	mt32sound_deinitialize();
-	mod = LoadLibrary(mt32sounddll);
-	if (mod == NULL) {
-		goto m3ini_err1;
+	m_hModule = ::LoadLibrary(s_szMT32SoundDll);
+	if (m_hModule == NULL)
+	{
+		return false;
 	}
-	mt32sound.mod = mod;
-	d = dllproc;
-	dterm = d + NELEMENTS(dllproc);
-	while(d < dterm) {
-		proc = (FARPROC)GetProcAddress(mod, d->symbol);
-		if (proc == (FARPROC)NULL) {
-			goto m3ini_err2;
+
+	//! ロード関数リスト
+	static const ProcItem s_dllProc[] =
+	{
+		{fn_mt32soundopen,	offsetof(MT32Sound, m_fnOpen)},
+		{fn_mt32soundclose,	offsetof(MT32Sound, m_fnClose)},
+		{fn_mt32soundwrite,	offsetof(MT32Sound, m_fnWrite)},
+		{fn_mt32soundmix,	offsetof(MT32Sound, m_fnMix)}
+	};
+
+	for (size_t i = 0; i < _countof(s_dllProc); i++)
+	{
+		FARPROC proc = ::GetProcAddress(m_hModule, s_dllProc[i].lpSymbol);
+		if (proc == NULL)
+		{
+			Deinitialize();
+			return false;
 		}
-		*(FARPROC *)(((UINT8 *)&mt32sound) + (d->addr)) = proc;
-		d++;
+		*(reinterpret_cast<FARPROC*>(reinterpret_cast<INT_PTR>(this) + s_dllProc[i].nOffset)) = proc;
 	}
-	return(SUCCESS);
-
-m3ini_err2:
-	mt32sound_deinitialize();
-
-m3ini_err1:
-	return(FAILURE);
+	return true;
 }
 
-void mt32sound_deinitialize(void) {
-
-	if (mt32sound.mod) {
-		FreeLibrary(mt32sound.mod);
+/**
+ * 解放
+ */
+void MT32Sound::Deinitialize()
+{
+	if (m_hModule)
+	{
+		::FreeLibrary(m_hModule);
 	}
-	ZeroMemory(&mt32sound, sizeof(mt32sound));
+	m_hModule = NULL;
+	m_bOpened = false;
+	m_fnOpen = NULL;
+	m_fnClose = NULL;
+	m_fnWrite = NULL;
+	m_fnMix = NULL;
 }
 
-BOOL mt32sound_isenable(void) {
-
-	return(mt32sound.mod != NULL);
-}
-
-void mt32sound_setrate(UINT rate) {
-
-	if (mt32sound.mod != NULL) {
-		mt32sound.rate = rate;
+/**
+ * オープン
+ * @retval true 成功
+ * @retval false 失敗
+ */
+bool MT32Sound::Open()
+{
+	if ((m_fnOpen != NULL) && (!m_bOpened) && (m_nRate))
+	{
+		(*m_fnOpen)(m_nRate, 0, 0, 0, 0, 0);
+		m_bOpened = true;
+		return true;
 	}
-}
-
-
-// ----
-
-BRESULT mt32sound_open(void) {
-
-	if ((mt32sound.mod != NULL) && (!mt32sound.opened) && (mt32sound.rate)) {
-		mt32sound.Open(mt32sound.rate, 0, 0, 0, 0, 0);
-		mt32sound.opened = TRUE;
-		return(SUCCESS);
-	}
-	else {
-		return(FAILURE);
+	else
+	{
+		return false;
 	}
 }
 
-void mt32sound_close(void) {
-
-	if (mt32sound.opened) {
-		if (mt32sound.mod != NULL) {
-			mt32sound.Close();
+/**
+ * クローズ
+ */
+void MT32Sound::Close()
+{
+	if (m_bOpened)
+	{
+		if (m_fnClose)
+		{
+			(*m_fnClose)();
 		}
-		mt32sound.opened = FALSE;
+		m_bOpened = false;
 	}
 }
 
-void mt32sound_shortmsg(UINT32 msg) {
+/**
+ * ショート メッセージ
+ * @param[in] msg メッセージ
+ */
+void MT32Sound::ShortMsg(UINT32 msg)
+{
+	int (WINAPI * fnWrite)(unsigned char data) = m_fnWrite;
+	if (fnWrite == NULL)
+	{
+		return;
+	}
 
-	int		(WINAPI * Write)(unsigned char data);
-
-	Write = mt32sound.Write;
-	switch((msg >> 4) & (0xf0 >> 4)) {
+	switch ((msg >> 4) & (0xf0 >> 4))
+	{
 		case 0xc0 >> 4:
 		case 0xd0 >> 4:
-			Write((UINT8)(msg >> 0));
-			Write((UINT8)(msg >> 8));
+			(*fnWrite)(static_cast<UINT8>(msg >> 0));
+			(*fnWrite)(static_cast<UINT8>(msg >> 8));
 			break;
 
 		case 0x80 >> 4:
@@ -132,45 +149,61 @@ void mt32sound_shortmsg(UINT32 msg) {
 		case 0xa0 >> 4:
 		case 0xb0 >> 4:
 		case 0xe0 >> 4:
-			Write((UINT8)(msg >> 0));
-			Write((UINT8)(msg >> 8));
-			Write((UINT8)(msg >> 16));
+			(*fnWrite)(static_cast<UINT8>(msg >> 0));
+			(*fnWrite)(static_cast<UINT8>(msg >> 8));
+			(*fnWrite)(static_cast<UINT8>(msg >> 16));
 			break;
 	}
 }
 
-void mt32sound_longmsg(const UINT8 *ptr, UINT32 leng) {
+/**
+ * ロング メッセージ
+ * @param[in] lpBuffer バッファ
+ * @param[in] cchBuffer バッファ長
+ */
+void MT32Sound::LongMsg(const UINT8* lpBuffer, UINT cchBuffer)
+{
+	if (lpBuffer == NULL)
+	{
+		return;
+	}
 
-	int		(WINAPI * Write)(unsigned char data);
+	int (WINAPI * fnWrite)(unsigned char data) = m_fnWrite;
+	if (fnWrite == NULL)
+	{
+		return;
+	}
 
-	Write = mt32sound.Write;
-	while(leng) {
-		Write(*ptr);
-		ptr++;
-		leng--;
+	for (UINT i = 0; i < cchBuffer; i++)
+	{
+		(*fnWrite)(lpBuffer[i]);
 	}
 }
 
-UINT mt32sound_mix32(SINT32 *buff, UINT leng) {
+/**
+ * ミックス
+ * @param[in,out] lpBuffer バッファ
+ * @param[in] cchBuffer サンプル数
+ * @return 出力サンプル数
+ */
+UINT MT32Sound::Mix(SINT32* lpBuffer, UINT cchBuffer)
+{
 
-	UINT	ret;
-	UINT	len;
-	SINT16	samp[512 * 2];
-	UINT	i;
-
-	ret = 0;
-	while(leng) {
-		len = min(leng, 512);
-		mt32sound.Mix(samp, len);
-		for (i=0; i<len; i++) {
-			buff[i*2+0] += ((SINT32)samp[i*2+0]) * 2;
-			buff[i*2+1] += ((SINT32)samp[i*2+1]) * 2;
+	UINT ret = 0;
+	while (cchBuffer)
+	{
+		SINT16 sSamples[512 * 2];
+		const UINT nLength = min(cchBuffer, 512);
+		(*m_fnMix)(sSamples, nLength);
+		for (UINT i = 0; i < nLength; i++)
+		{
+			lpBuffer[i * 2 + 0] += sSamples[i * 2 + 0] * 2;
+			lpBuffer[i * 2 + 1] += sSamples[i * 2 + 1] * 2;
 		}
-		buff += len * 2;
-		leng -= len;
-		ret += len;
+		lpBuffer += nLength * 2;
+		cchBuffer -= nLength;
+		ret += nLength;
 	}
-	return(ret);
+	return ret;
 }
 #endif
-
--- reform_ui/win9x/mt32snd.h
+++ reform_ui/win9x/mt32snd.h
@@ -1,30 +1,132 @@
+/**
+ * @file	mt32snd.h
+ * @brief	MT32Sound アクセス クラスの宣言およびインターフェイスの定義をします
+ */
+
+#pragma once
 
 #if defined(MT32SOUND_DLL)
 
-typedef struct {
-	void	(*shortmsg)(UINT32 msg);
-	void	(*longmsg)(const UINT8 *ptr, UINT32 leng);
-	UINT	(*mix32)(SINT32 *buff, UINT leng);
-} _MT32HDL, *MT32HDL;
+/**
+ * @brief MT32Sound アクセス クラス
+ */
+class MT32Sound
+{
+public:
+	static MT32Sound* GetInstance();
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+	MT32Sound();
+	~MT32Sound();
+	bool Initialize();
+	void Deinitialize();
+	bool IsEnabled() const;
+	void SetRate(UINT nRate);
 
-BRESULT mt32sound_initialize(void);
-void mt32sound_deinitialize(void);
-BOOL mt32sound_isenable(void);
-void mt32sound_setrate(UINT rate);
+	bool Open();
+	void Close();
+	void ShortMsg(UINT32 msg);
+	void LongMsg(const UINT8* lpBuffer, UINT cchBuffer);
+	UINT Mix(SINT32* lpBuffer, UINT cchBuffer);
 
-BRESULT mt32sound_open(void);
-void mt32sound_close(void);
-void mt32sound_shortmsg(UINT32 msg);
-void mt32sound_longmsg(const UINT8 *ptr, UINT32 leng);
-UINT mt32sound_mix32(SINT32 *buff, UINT leng);
+private:
+	static MT32Sound sm_instance;	//!< 唯一のインスタンスです
 
-#ifdef __cplusplus
+	//! @brief ロード関数
+	struct ProcItem
+	{
+		LPCSTR lpSymbol;			//!< 関数名
+		size_t nOffset;				//!< オフセット
+	};
+
+	// 定義
+	typedef int (WINAPI * FnOpen)(int rate, int reverb, int def, int revtype, int revtime, int revlvl); 	/*!< オープン */
+	typedef int (WINAPI * FnClose)(void);																	/*!< クローズ */
+	typedef int (WINAPI * FnWrite)(unsigned char data);														/*!< ライト */
+	typedef int (WINAPI * FnMix)(void *buff, unsigned long size);											/*!< ミックス */
+
+	HMODULE m_hModule;	/*!< モジュール */
+	bool m_bOpened;		/*!< オープン フラグ */
+	UINT m_nRate;		/*!< サンプリング レート */
+	FnOpen m_fnOpen;	/*!< オープン関数 */
+	FnClose m_fnClose;	/*!< クローズ関数 */
+	FnWrite m_fnWrite;	/*!< ライト関数 */
+	FnMix m_fnMix;		/*!< ミックス関数 */
+};
+
+/**
+ * インスタンスを取得
+ * @return インスタンス
+ */
+inline MT32Sound* MT32Sound::GetInstance()
+{
+	return &sm_instance;
 }
-#endif
+
+/**
+ * 有効?
+ * @retval true 有効
+ * @retval false 無効
+ */
+inline bool MT32Sound::IsEnabled() const
+{
+	return (m_hModule != NULL);
+}
+
+/**
+ * サンプリング レートを設定
+ * @param[in] nRate レート
+ */
+inline void MT32Sound::SetRate(UINT nRate)
+{
+	m_nRate = nRate;
+}
+
+
+// ----
+
+inline BRESULT mt32sound_initialize(void)
+{
+	return MT32Sound::GetInstance()->Initialize() ? SUCCESS : FAILURE;
+}
+
+inline void mt32sound_deinitialize()
+{
+	MT32Sound::GetInstance()->Deinitialize();
+}
+
+inline BOOL mt32sound_isenable(void)
+{
+	return MT32Sound::GetInstance()->IsEnabled() ? TRUE : FALSE;
+}
+
+inline void mt32sound_setrate(UINT rate)
+{
+	MT32Sound::GetInstance()->SetRate(rate);
+}
+
+inline BRESULT mt32sound_open(void)
+{
+	return MT32Sound::GetInstance()->Open() ? SUCCESS : FAILURE;
+}
+
+inline void mt32sound_close(void)
+{
+	MT32Sound::GetInstance()->Close();
+}
+
+inline void mt32sound_shortmsg(UINT32 msg)
+{
+	MT32Sound::GetInstance()->ShortMsg(msg);
+}
+
+inline void mt32sound_longmsg(const UINT8 *ptr, UINT32 leng)
+{
+	MT32Sound::GetInstance()->LongMsg(ptr, leng);
+}
+
+inline UINT mt32sound_mix32(SINT32 *buff, UINT leng)
+{
+	return MT32Sound::GetInstance()->Mix(buff, leng);
+}
 
 #endif
-
