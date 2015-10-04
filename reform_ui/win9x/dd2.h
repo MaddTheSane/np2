f0dd29cdd4069caae8fc2feafc01c38e45672e2f
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2015-10-04 23:21:12 +0000

    ✓ win9x: subwnd
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@2118 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- reform_ui/win9x/dd2.h
+++ reform_ui/win9x/dd2.h
@@ -1,9 +1,46 @@
 
-#include	"cmndraw.h"
 
-#define	DD2HDL		void *
+#pragma once
+
+#include <ddraw.h>
+#include "cmndraw.h"
+
+/**
+ * @brief DirectDraw2 class
+ */
+class DD2Surface
+{
+public:
+	DD2Surface();
+	~DD2Surface();
+
+	bool Create(HWND hWnd, int nWidth, int nHeight);
+	void Release();
+	CMNVRAM* Lock();
+	void Unlock();
+	void Blt(const POINT *pt, const RECT *rect);
+	UINT16 GetPalette16(RGB32 pal) const;
+
+protected:
+	HWND					hwnd;
+	LPDIRECTDRAW			ddraw1;
+	LPDIRECTDRAW2			ddraw;
+	LPDIRECTDRAWSURFACE		primsurf;
+	LPDIRECTDRAWSURFACE		backsurf;
+	DDPIXELFORMAT			ddpf;
+	LPDIRECTDRAWCLIPPER		clipper;
+	LPDIRECTDRAWPALETTE		palette;
+	int						cliping;
+	RGB32					pal16;
+	UINT8					r16b;
+	UINT8					l16r;
+	UINT8					l16g;
+	CMNVRAM					vram;
+	PALETTEENTRY			pal[256];
+};
 
 
+#define DD2HDL DD2Surface*
 DD2HDL dd2_create(HWND hwnd, int width, int height);
 void dd2_release(DD2HDL dd2hdl);
 CMNVRAM *dd2_bsurflock(DD2HDL dd2);
