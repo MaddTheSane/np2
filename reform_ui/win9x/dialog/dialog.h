da9c0c07a479f839b4eb8b718117e8de02c81792
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2016-01-26 10:40:43 +0000

    ✓ win9x: disk
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@2432 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- reform_ui/win9x/dialog/dialog.h
+++ reform_ui/win9x/dialog/dialog.h
@@ -12,12 +12,14 @@ void dialog_sndopt(HWND hWnd);
 void dialog_mpu98(HWND hwndParent);
 void dialog_serial(HWND hWnd);
 void dialog_calendar(HWND hwndParent);
-void dialog_newdisk(HWND hWnd);
-void dialog_changefdd(HWND hWnd, REG8 drv);
-void dialog_changehdd(HWND hWnd, REG8 drv);
 
 // d_bmp.cpp
 void dialog_writebmp(HWND hWnd);
+
+// d_disk.cpp
+void dialog_changefdd(HWND hWnd, REG8 drv);
+void dialog_changehdd(HWND hWnd, REG8 drv);
+void dialog_newdisk(HWND hWnd);
 
 // d_font.cpp
 void dialog_font(HWND hWnd);
