2c86cabe56203be95ff0000e3202504d1225206a
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2016-02-16 06:29:56 +0000

    ✓ x11: supports UTF-8 refs #129
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/branches/yui@2519 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- reform_ui/x11/oemtext.h
+++ reform_ui/x11/oemtext.h
@@ -1,7 +1,15 @@
+#ifndef	NP2_X11_OEMTEXT_H__
+#define	NP2_X11_OEMTEXT_H__
+
 #include "codecnv/codecnv.h"
 
+#if defined(OSLANG_UTF8)
+#define	oemtext_sjistooem	codecnv_sjistoutf8
+#define	oemtext_oemtosjis	codecnv_utf8tosjis
+#elif defined(OSLANG_EUC)
 #define	oemtext_sjistooem	codecnv_sjistoeuc
 #define	oemtext_oemtosjis	codecnv_euctosjis
+#endif
 
 #ifdef __cplusplus
 #include <string>
@@ -10,3 +18,5 @@ namespace std
 	typedef string oemstring;
 }
 #endif  /* __cplusplus */
+
+#endif	/* NP2_X11_OEMTEXT_H__ */
