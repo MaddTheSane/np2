996690597d0a5465d46dea9c223e7d7d71705f8b
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2016-03-24 12:54:35 +0000

    PC-9801-86: SoundID of DIPSW refs #138  svn merge -r 2608:2609 https://amethyst.yui.ne.jp/svn-dev/pc98/np2/branches/yui/WORK_01
    git-svn-id: http://amethyst.yui.ne.jp/svn/pc98/np2/trunk@2615 7c2116fe-53d0-4aec-ab59-80607f05abc0

--- statsave.c
+++ statsave.c
@@ -918,7 +918,7 @@ static int flagload_fm(STFLAGH sfh, const SFENTRY *tbl)
 	pcm86gen_update();
 	if (nSaveFlags & FLAG_PCM86)
 	{
-		fmboard_extenable((REG8)(g_pcm86.extfunc & 1));
+		fmboard_extenable((REG8)(g_pcm86.soundflags & 1));
 	}
 	if (nSaveFlags & FLAG_CS4231)
 	{
