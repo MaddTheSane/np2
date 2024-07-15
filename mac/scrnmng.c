ad9ecacbb76ef757acd84045df18a35098e65a93
C.W. Betts <computers57@hotmail.com>
2024-07-15 17:42:54 -0600

    Minor pokes.

--- mac/scrnmng.c
+++ mac/scrnmng.c
@@ -7,3 +7,28 @@
 //
 
 #include "scrnmng.h"
+
+RGB16 scrnmng_makepal16(RGB32 pal32)
+{
+	return 0;
+}
+
+void scrnmng_setwidth(int posx, int width)
+{
+	
+}
+
+void scrnmng_setheight(int posy, int height)
+{
+	
+}
+
+const SCRNSURF *scrnmng_surflock(void)
+{
+	return NULL;
+}
+
+void scrnmng_surfunlock(const SCRNSURF *surf)
+{
+	
+}
--- mac/soundmng.c
+++ mac/soundmng.c
@@ -7,3 +7,23 @@
 //
 
 #include "soundmng.h"
+
+UINT soundmng_create(UINT rate, UINT ms)
+{
+	return 0;
+}
+
+void soundmng_destroy(void)
+{
+	
+}
+
+void soundmng_play(void)
+{
+	
+}
+
+void soundmng_stop(void)
+{
+	
+}
